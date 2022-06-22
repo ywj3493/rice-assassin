82cb3d27ab29ae35050e45b78d9f2947c3cf963c
fifteenmania <ktthee1995@gmail.com>
2022-06-22 13:07:05 +0900

    docs: comments on lib, hook functions

--- src/hook/useCascadingCategory.ts
+++ src/hook/useCascadingCategory.ts
@@ -1,8 +1,24 @@
 import Category from "../class/Category"
 
-export default function useCascadingCategory() {
+export enum CategoryLevel {
+  THEME = 'theme',
+  LARGE_CATEGORY = 'large_category',
+  MEDIUM_CATEGORY = 'medium_category',
+  SMALL_CATEGORY = 'small_category'
+}
+
+
+export default function useCascadingCategory(): [
+  currentLevel: CategoryLevel,
+  currentKey: Category| null,
+  getChildren: () => Category[],
+  chooseChild: (childKey: string) => void,
+  chooseRandom: () => void
+] {
   const getChildren: () => Category[] = () => []
   const chooseChild: (childKey: string) => void = () => {}
   const chooseRandom: () => void = () => {}
-  return ["theme", null, getChildren, chooseChild, chooseRandom]
+  return [CategoryLevel.THEME, null, getChildren, chooseChild, chooseRandom]
 }
+
+useCascadingCategory()
--- src/lib/categoryGetters.ts
+++ src/lib/categoryGetters.ts
@@ -3,18 +3,37 @@ import MediumCategory from "../class/MediumCategory";
 import SmallCategory from "../class/SmallCategory";
 import Theme from "../class/Theme";
 
+/**
+ * 
+ * @returns 전체 주제 리스트
+ */
 export function getThemeList(): Theme[] {
   return []
 }
 
+/**
+ * 
+ * @param themeKey 상위 주제의 키
+ * @returns 해당 테마를 부모로 가지는 대분류의 리스트. 대분류의 키가 유효하지 않으면 빈 리스트 반환.
+ */
 export function getLargeCategoryList(themeKey: string): LargeCategory[] {
   return []
 }
 
+/**
+ * 
+ * @param largetCategoryKey 상위 대분류의 키
+ * @returns 해당 대분류를 부모로 가지는 중분류의 리스트. 대분류의 키가 유효하지 않으면 빈 리스트 반환.
+ */
 export function getMediumCategoryList(largetCategoryKey: string): MediumCategory[] {
   return []
 }
 
+/**
+ * 
+ * @param mediumCategory 상위 중분류의 키
+ * @returns 해당 중분류를 부모로 가지는 소분류의 리스트. 중분류의 키가 유효하지 않으면 빈 리스트 반환.
+ */
 export function getSmallCategoryList(mediumCategory: string): SmallCategory[] {
   return []
 }
