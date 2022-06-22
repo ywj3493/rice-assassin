5b26d1034ddb89a3389045c62d73f484208157a1
fifteenmania <ktthee1995@gmail.com>
2022-06-22 17:56:31 +0900

    fix: currentLevel, currentKey types, typo

--- src/class/Category.ts
+++ src/class/Category.ts
@@ -1,3 +1,7 @@
+/**
+ * 카테고리성 데이터의 상위 클래스.
+ * key: 해당 카테고리의 고유 식별자. 같은 레벨의 카테고리 안에서 유일해야 함.
+ */
 export default interface Category {
   key: string;
 }
--- src/hook/useCascadingCategory.ts
+++ src/hook/useCascadingCategory.ts
@@ -3,25 +3,27 @@ import Category from "../class/Category"
 /**
  * 해당 카테고리의 레벨. string 형태로 저장
  */
-export enum CategoryLevel {
-  THEME = 'theme',
-  LARGE_CATEGORY = 'large_category',
-  MEDIUM_CATEGORY = 'medium_category',
-  SMALL_CATEGORY = 'small_category'
-}
+export const CategoryLevel = {
+  THEME: 'theme',
+  LARGE_CATEGORY: 'large_category',
+  MEDIUM_CATEGORY: 'medium_category',
+  SMALL_CATEGORY: 'small_category'
+} as const
+
+
 
 /**
  * @returns [  
- * `currentLevel` 현재 카테고리의 레벨,  
- * `currentKey` 현재 카테고리의 키,  
+ * `currentLevel` 현재 선택된 카테고리의 레벨,  
+ * `currentCategory` 현재 선택된 카테고리 객체 (없으면 null 반환),  
  * `getChildren` 현재 카테고리 하위의 카테고리를 반환하는 함수,  
  * `chooseChild` 하위 카테고리의 키를 인자로 받아 현재 카테고리로 선택하는 함수, 
  * `chooseRandom` 하위 카테고리 중 하나를 랜덤으로 선택하는 함수  
  * ] 
  */
 export default function useCascadingCategory(): [
-  currentLevel: CategoryLevel,
-  currentKey: Category| null,
+  currentLevel: typeof CategoryLevel[keyof typeof CategoryLevel],
+  currentCategory: Category | null,
   getChildren: () => Category[],
   chooseChild: (childKey: string) => void,
   chooseRandom: () => void
@@ -29,5 +31,5 @@ export default function useCascadingCategory(): [
   const getChildren: () => Category[] = () => []
   const chooseChild: (childKey: string) => void = () => {}
   const chooseRandom: () => void = () => {}
-  return [CategoryLevel.THEME, null, getChildren, chooseChild, chooseRandom]
+  return ["theme", null, getChildren, chooseChild, chooseRandom]
 }
