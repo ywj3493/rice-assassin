a6d2f512916da31ac4c93774304f5ae33f0f7c28
fifteenmania <ktthee1995@gmail.com>
2022-06-22 16:54:49 +0900

    docs: edit useCascaseCategory comments

--- src/hook/useCascadingCategory.ts
+++ src/hook/useCascadingCategory.ts
@@ -10,7 +10,15 @@ export enum CategoryLevel {
   SMALL_CATEGORY = 'small_category'
 }
 
-
+/**
+ * @returns [  
+ * `currentLevel` 현재 카테고리의 레벨,  
+ * `currentKey` 현재 카테고리의 키,  
+ * `getChildren` 현재 카테고리 하위의 카테고리를 반환하는 함수,  
+ * `chooseChild` 하위 카테고리의 키를 인자로 받아 현재 카테고리로 선택하는 함수, 
+ * `chooseRandom` 하위 카테고리 중 하나를 랜덤으로 선택하는 함수  
+ * ] 
+ */
 export default function useCascadingCategory(): [
   currentLevel: CategoryLevel,
   currentKey: Category| null,
@@ -23,5 +31,3 @@ export default function useCascadingCategory(): [
   const chooseRandom: () => void = () => {}
   return [CategoryLevel.THEME, null, getChildren, chooseChild, chooseRandom]
 }
-
-useCascadingCategory()
