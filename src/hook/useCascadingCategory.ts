cbcfa43ab268ff0f0c7d08293f53b46d244f2238
fifteenmania <ktthee1995@gmail.com>
2022-06-22 13:07:05 +0900

    docs: comments on lib, hook functions

--- src/hook/useCascadingCategory.ts
+++ src/hook/useCascadingCategory.ts
@@ -1,8 +1,25 @@
 import Category from "../class/Category"
 
-export default function useCascadingCategory() {
+/**
+ * 해당 카테고리의 레벨. string 형태로 저장
+ */
+export enum CategoryLevel {
+  THEME = 'theme',
+  LARGE_CATEGORY = 'large_category',
+  MEDIUM_CATEGORY = 'medium_category',
+  SMALL_CATEGORY = 'small_category'
+}
+
+
+export default function useCascadingCategory(): [
+  currentLevel: CategoryLevel,
+  currentKey: Category| null,
+  getChildren: () => Category[],
+  chooseChild: (childKey: string) => void,
+  chooseRandom: () => void
+] {
   const getChildren: () => Category[] = () => []
   const chooseChild: (childKey: string) => void = () => {}
   const chooseRandom: () => void = () => {}
-  return ["theme", null, getChildren, chooseChild, chooseRandom]
+  return [CategoryLevel.THEME, null, getChildren, chooseChild, chooseRandom]
 }
