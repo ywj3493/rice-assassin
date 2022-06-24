cf60af3eda39ab0815dbfc2f1522ad45d4bdaeb9
fifteenmania <ktthee1995@gmail.com>
2022-06-24 13:30:26 +0900

    feat: fake category repositories

---
+++ src/class/FakeCategoryRepository.test.ts
@@ -0,0 +1,47 @@
+import FakeCategoryRepository from "./FakeCategoryRepository"
+
+describe("fake category repository generation", () => {
+  it("initallization", () => {
+    const repo = new FakeCategoryRepository(0, 0, 0, 0)
+    expect(repo).toBeInstanceOf(FakeCategoryRepository)
+    expect(repo.themeList).toEqual([])
+  })
+
+  it("only one theme", () => {
+    const repo = new FakeCategoryRepository(1, 0, 0, 0)
+    expect(repo.themeList.length).toBe(1)
+    expect(repo.themeList[0]).toHaveProperty("key")
+    expect(repo.themeList[0]).toHaveProperty("name")
+  })
+
+  it("two themes", () => {
+    const repo = new FakeCategoryRepository(2, 0, 0, 0)
+    expect(repo.themeList.length).toBe(2)
+  })
+
+  it("with subcategory", () => {
+    const repo = new FakeCategoryRepository(2, 2, 1, 1)
+    expect(repo.themeList.length).toBe(2)
+    expect(repo.largeCategoryList.length).toBe(4)
+    expect(repo.mediumCategoryList.length).toBe(4)
+    expect(repo.smallCategoryList.length).toBe(4)
+  })
+
+  it("with many subcategory", () => {
+    const repo = new FakeCategoryRepository(10, 10, 10, 10)
+    expect(repo.themeList.length).toBe(10)
+    expect(repo.largeCategoryList.length).toBe(100)
+    expect(repo.mediumCategoryList.length).toBe(1000)
+    expect(repo.smallCategoryList.length).toBe(10000)
+  })
+
+  it("get theme child", () => {
+    const repo = new FakeCategoryRepository(10, 10, 0, 0)
+    const parent = repo.getRandomTheme() 
+    const children = repo.getThemeChildren(parent.key)
+    expect(repo.largeCategoryList.length).toBe(100)
+    expect(children.length).toBe(10)
+    expect(children.every((largeCategory) => largeCategory.parentKey === parent.key))
+  })
+})
+
---
+++ src/class/FakeCategoryRepository.ts
@@ -0,0 +1,54 @@
+import Category from "../interface/Category";
+import ChildCategory from "../interface/ChildCategory";
+import LargeCategory from "../interface/LargeCategory";
+import MediumCategory from "../interface/MediumCategory";
+import SmallCategory from "../interface/SmallCategory";
+import ThemeCategory from "../interface/ThemeCategory";
+import { makeFakeLargeCategory, makeFakeMediumCategory, makeFakeSmallCategory, makeFakeThemeCategory } from "../lib/makeFakeCategory";
+import { randomPick } from "../lib/randUtil";
+
+// 이 모듈은 개발 모드에서만 사용해야 함
+
+export default class FakeCategoryRepository {
+  themeList: ThemeCategory[] = [];
+  largeCategoryList: LargeCategory[] = [];
+  mediumCategoryList: MediumCategory[] = [];
+  smallCategoryList: SmallCategory[] = [];
+
+  constructor(themeLen: number, largeCategoryLen: number, mediumCategoryLen: number, smallCategoryLen: number) {
+    Array.from({length: themeLen}).forEach(() => this.themeList.push(makeFakeThemeCategory()));
+    this.themeList.forEach((theme) => Array.from({length: largeCategoryLen}).forEach(() => 
+      this.largeCategoryList.push(makeFakeLargeCategory(theme.key))
+    ))
+    this.largeCategoryList.forEach((largeCategory) => Array.from({length: mediumCategoryLen}).forEach(() => 
+      this.mediumCategoryList.push(makeFakeMediumCategory(largeCategory.key))
+    ))
+    this.mediumCategoryList.forEach((mediumCategory) => Array.from({length: smallCategoryLen}).forEach(() => {
+      this.smallCategoryList.push(makeFakeSmallCategory(mediumCategory.key))
+    }))
+  }
+
+  getRandomTheme(): ThemeCategory {
+    return randomPick(this.themeList)
+  }
+
+  getCategoryChildren(level: number, parentKey: string): ChildCategory[] {
+    if (level > 3) {
+      throw new Error("invalid level")
+    }
+    const lists: ChildCategory[][] = [[], this.largeCategoryList, this.mediumCategoryList, this.smallCategoryList]
+    return lists[level].filter((category) => category.parentKey === parentKey)
+  }
+
+  getThemeChildren(themeCategoryKey: string): LargeCategory[] {
+    return this.largeCategoryList.filter((largeCategory) => largeCategory.parentKey === themeCategoryKey)
+  }
+
+  getLargeCategoryChildren(largeCategoryKey: string): MediumCategory[] {
+    return this.mediumCategoryList.filter((mediumCategory) => mediumCategory.parentKey === largeCategoryKey)
+  }
+
+  getMediumCategoryChildren(mediumCategoryKey: string): SmallCategory[] {
+    return this.smallCategoryList.filter((smallCategory) => smallCategory.parentKey === mediumCategoryKey)
+  }
+}
