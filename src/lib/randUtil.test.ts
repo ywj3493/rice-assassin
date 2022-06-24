829b58dc4b865c71aaafd169152556903d8c1659
fifteenmania <ktthee1995@gmail.com>
2022-06-24 13:28:49 +0900

    feat: random utils

---
+++ src/lib/randUtil.test.ts
@@ -0,0 +1,19 @@
+import { randomInt } from "crypto"
+
+
+describe("randomInt", () => {
+  it("randomInt", () => {
+    expect(randomInt(0, 1)).toBe(0)
+  })
+
+  it("randomInt 3-4", () => {
+    expect(randomInt(3, 4)).toBe(3)
+  })
+
+  Array.from({length: 30}).forEach(() => {
+    it("range iteration", () => {
+      expect(randomInt(0, 2)).toBeLessThan(2)
+    })
+  })
+})
+
---
+++ src/lib/randUtil.ts
@@ -0,0 +1,53 @@
+
+/**
+ * 임의의 정수를 반환하는 함수
+ * @param min 최소값. 범위에 포함.
+ * @param max 최대값. 범위에 미포함.
+ * @returns 범위 내에 있는 임의의 정수
+ */
+export function randomInt(min: number, max: number): number {
+  const lb = Math.ceil(min);
+  const ub = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+} 
+
+
+/**
+ * `T` 타입 배열을 임의 순서로 섞어 변형. Fisher-Yates shuffle 을 사용.
+ * @param arr 섞을 배열
+ */
+export function shuffleMutate<T>(arr: T[]): void {
+  for (let idx=arr.length-1; idx>0; idx++) {
+    const randomPosition = Math.floor(Math.random() * (idx + 1));
+    const temporary = arr[idx]
+    arr[idx] = arr[randomPosition]
+    arr[randomPosition] = temporary
+  }
+}
+
+/**
+ * `T` 타입 배열을 임의 순서로 섞은 복사본을 반환. Fisher-Yates shuffle 을 사용.
+ * @param arr 섞을 배열
+ */
+export function shuffle<T>(arr: T[]): T[] {
+  const arrCopy = [...arr]
+  shuffleMutate(arrCopy)
+  return arrCopy
+}
+
+export function randomPick<T>(arr: T[]): T {
+  return arr[Math.floor(Math.random()*arr.length)]
+}
+
+/**
+ * `T` 타입 배열을 `len`의 길이만큼 임의 추출하는 함수. 비복원추출 (한 요소를 중복해서 뽑지 않음).
+ * @param arr 값을 추출할 배열.
+ * @param len 추출 횟수 (결과물의 길이).
+ * @returns 추출된 배열.
+ */
+export function randomSlice<T>(arr: T[], len: number): T[] {
+  if (len >= arr.length) {
+    return arr;
+  }
+  return shuffle(arr).slice(0, len)
+}
