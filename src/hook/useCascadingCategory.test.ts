26c902c72e7bb3696aae41a7e35eb592d7bb53c5
fifteenmania <ktthee1995@gmail.com>
2022-06-27 15:49:15 +0900

    feat: implement useCascadingCategory hook with actual test data

--- src/hook/useCascadingCategory.test.ts
+++ src/hook/useCascadingCategory.test.ts
@@ -1,16 +1,16 @@
 import { act, renderHook } from "@testing-library/react"
-import useCascadingCategory from "./useCascadingCategory"
+import useCascadingCategory, {CategoryLevel, useMockCascadingCategory} from "./useCascadingCategory"
 
-describe("useCascadingCategory init", () => {
+describe("useMockCascadingCategory", () => {
   it("category initialization", () => {
-    const {result} = renderHook(() => useCascadingCategory())
+    const {result} = renderHook(() => useMockCascadingCategory())
     const [currentLevel, currentCategory] = result.current
-    expect(currentLevel).toEqual(0)
+    expect(currentLevel).toEqual(CategoryLevel.NOTHING)
     expect(currentCategory).toBeNull()
   })
 
   it("selection", () => {
-    const {result} = renderHook(() => useCascadingCategory())
+    const {result} = renderHook(() => useMockCascadingCategory())
     const [currentLevel, currentCategory, getChildren, chooseChild] = result.current
     const children = getChildren()
     act(() => chooseChild(children[0].key))
@@ -18,17 +18,35 @@ describe("useCascadingCategory init", () => {
     expect(result.current[0]).toBe(1)
   })
 
-
   it("random selection", () => {
-    const {result} = renderHook(() => useCascadingCategory())
+    const {result} = renderHook(() => useMockCascadingCategory())
     const [currentLevel, currentCategory, getChildren, chooseChild, chooseRandom] = result.current
     const children = getChildren()
     act(() => chooseRandom())
-    expect(result.current[0]).toBe(1)
+    expect(result.current[0]).toBe(CategoryLevel.THEME)
     act(() => result.current[4]())
-    expect(result.current[0]).toBe(2)
+    expect(result.current[0]).toBe(CategoryLevel.LARGE_CATEGORY)
     act(() => result.current[4]())
-    expect(result.current[0]).toBe(3)
+    expect(result.current[0]).toBe(CategoryLevel.MEDIUM_CATEGORY)
   })
 })
+
+describe("useCascadingCategory", () => {
+  it("initialization", () => {
+    const {result} = renderHook(() => useCascadingCategory())
+    const [currentLevel, currentCategory, getChildren, chooseChild, chooseRandom] = result.current
+    expect(currentLevel).toEqual(CategoryLevel.NOTHING)
+    expect(currentCategory).toBeNull()
+  })
+
+  it("theme read", () => {
+    const {result} = renderHook(() => useCascadingCategory())
+    const [currentLevel, currentCategory, getChildren, chooseChild, chooseRandom] = result.current
+    const child = getChildren()[0]
+    act(() => chooseChild(child.key))
+    expect(result.current[0]).toBe(CategoryLevel.THEME)
+    expect(result.current[1]?.key).toEqual(child.key)
+  })
+})
+
 
