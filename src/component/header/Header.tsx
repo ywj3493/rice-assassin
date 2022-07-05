80c2f82434e52119a43994e3cefa6707c612ebfe
ywj3493 <ywj3493@nate.com>
2022-07-05 16:26:27 +0900

    [feat] add Header

--- src/component/header/Header.tsx
+++ src/component/header/Header.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 
 const Header = () => (
-  <section className={`bg-ra-100 flex justify-center p-12`}>
-    <h1>오늘의 밥도둑</h1>
+  <section className={`p-12`}>
+    <div className={`flex items-center flex-col`}>
+      <h1 className={`text-2xl`}>오늘의 밥도둑</h1>
+    </div>
+    <div className={`bg-slate-100 h-[2px] rounded-full my-4`}></div>
   </section>
 );
 
